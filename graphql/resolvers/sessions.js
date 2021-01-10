67d3b358d857404094cb1d57832c7fa70278f7ad
kabilank6 <kabilan-k@hotmail.com>
2021-01-10 23:05:43 +1300

    Enhance Sessions resolvers

--- graphql/resolvers/sessions.js
+++ graphql/resolvers/sessions.js
@@ -10,3 +10,56 @@ const user = async userId => {
     }
 }
 
+module.exports = {
+    sessions: async (args) => {
+        let sessions;
+        // Attempt to find the goal given its id
+        if (args.id != null){
+            sessions = await Session.findById(args.id);
+            sessions = [sessions]
+        } else {
+            sessions = await Session.find();
+        }
+        try {
+            return sessions.map(session => {
+                return {
+                    ...session._doc,
+                    goal: user.bind(this, session._doc.goal),
+                    user: user.bind(this, goal._doc.user)
+                };
+            });
+        } catch (err) {
+            console.log(err);
+            throw err;
+        }
+    },
+    createSession: async (args) => {
+        const session = new Session({
+            user: args.sessionInput.user,
+            goal: args.sessionInput.goal,
+        });
+
+        let createdSession;
+        try {
+            const result = await session.save();
+            createdSession = { ...sesh._doc };
+
+            // Find the creator and add this goal reference to them.
+            const creator = await User.findById(args.sessionInput.user);
+            if (!creator) {
+                throw new Error("No user found");
+            }
+            // console.log(user)
+            user.createdSessions.push(session);
+            user.save();
+
+            return createdGoal;
+        } catch (err) {
+            console.log(err);
+            throw err;
+        }
+    
+
+    }
+
+}
